9184b6974d5d7ce5765a9e131e82021dfc8fbff3
Einstein.dev <einstein.dev@hotmail.com>
2024-04-17 22:29:11 -0300

    feature :  inserindo skeleton

---
+++ src/components/common/Skeleton/Skeleton.tsx
@@ -0,0 +1,6 @@
+import SkeletoLib, { SkeletonProps } from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export function Skeleton(props: SkeletonProps){
+	return <SkeletoLib {...props}/>
+}
--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from './Pets.module.css'
+import { Card } from "../../components/common/Card";
+import { getPets } from "../../services/pets/getPets";
+
+import { Skeleton } from "../../components/common/Skeleton";
+
 
 export function Pets(){
+	const [pets, setPets] = useState([])
+	const [isLoading]
+
+	useEffect(()=> {
+		async function loadData() {
+			const data = await getPets()
+			setPets(data.items)
+			
+		}
+		loadData()
+	}, [])
 	return (
 		<Grid>
 			<div className={styles.container}>
 				<Header/>
+				<main className={styles.list}>
+					<Skeleton count={5} containerClassName={styles.skeleton}/>
+					{ 	pets.map(pet => (
+							< 	key={pet.id}
+									href=`/pets/${pet.id}` 
+									text={pet.name} 
+									thumb={pet.photo}/>
+						))
+					}
+				</main>
 				<Link to="/pets/20">Ir para listagem</Link>
 			</div>
 		</Grid>)
