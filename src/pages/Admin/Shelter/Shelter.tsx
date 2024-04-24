7e70233b6f4c10341ffd0bd3b6c08b17bb770977
Einstein.dev <einstein.dev@hotmail.com>
2024-04-24 19:59:33 -0300

    feature : inserindo AuthHOC, sideBar

---
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -0,0 +1,19 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
+import { Panel } from "../../../components/layout/Panel";
+import styles from './Shelter.module.css'
+
+export function Shelter(){
+	function submit(event){
+		event.preventDefault()
+		console.log(inputRef.current.value)
+	}
+	return (
+	<Panel>
+		<form className={styles.container}>
+			<Input ref={inputRef} label="Nome"/>
+			<Button type="submit">Salvar dados</Button>
+		</form>
+	</Panel>
+	)
+}
